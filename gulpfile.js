75e11ea48f219091ec1a47b43ca537f0969b9231
Kris Bastiani <kris.bastiani@gmail.com>
2019-07-27 20:53:09 +1000

    Update Gulp configuration

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,7 @@
 const gulp = require('gulp');
 
-gulp.task('build', [
-	'fonts',
-	'images',
-	'pug',
-	'sass',
-	'webpack',
-]);
+gulp.task('build', require('./lib/tasks/build'));
+gulp.task('clean', require('./lib/tasks/clean'));
 gulp.task('connect', require('./lib/tasks/connect'));
 gulp.task('fonts', require('./lib/tasks/fonts'));
 gulp.task('images', require('./lib/tasks/images'));
@@ -14,6 +9,14 @@ gulp.task('pug', require('./lib/tasks/pug'));
 gulp.task('sass', require('./lib/tasks/sass'));
 gulp.task('watch', require('./lib/tasks/watch'));
 gulp.task('webpack', require('./lib/tasks/webpack'));
+
+gulp.task('transpile', [
+	'fonts',
+	'images',
+	'pug',
+	'sass',
+	'webpack',
+]);
 
 gulp.task('default', [
 	'build',
