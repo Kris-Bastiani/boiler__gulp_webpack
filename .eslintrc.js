5272a2f3320b7bebc7d11095c52bf6284038720a
Kris Bastiani <kris.bastiani@gmail.com>
2019-07-27 20:52:38 +1000

    Update ESLint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,43 @@
 module.exports = {
-	extends: 'airbnb-base',
+	env: { browser: true },
+	extends: ['airbnb-base'],
+	parserOptions: { ecmaVersion: 8 },
+
 	rules: {
-		indent: ['error', 'tab'],
+		'no-confusing-arrow': 0,
+		'arrow-parens': 0,
 		'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+		'import/no-amd': 0,
+		'import/no-named-as-default': 0,
+		'import/no-named-as-default-member': 0,
+		indent: ['error', 'tab'],
+		'class-methods-use-this': 0,
 		'no-mixed-operators': [
+		'error',
+		{
+			groups: [
+				['+', '-', '*', '/', '%', '**'],
+				['&', '|', '^', '~', '<<', '>>', '>>>'],
+				['==', '!=', '===', '!==', '>', '>=', '<', '<='],
+				['&&', '||'],
+				['in', 'instanceof'],
+			],
+		},
+		],
+		'no-param-reassign': [
 			'error',
 			{
-				groups: [
-					['+', '-', '*', '/', '%', '**'],
-					['&', '|', '^', '~', '<<', '>>', '>>>'],
-					['==', '!=', '===', '!==', '>', '>=', '<', '<='],
-					['&&', '||'],
-					['in', 'instanceof'],
+				props: true,
+				ignorePropertyModificationsFor: [
+					'acc', // for reduce accumulators
+					'accumulator', // for reduce accumulators
+					'e', // for e.returnvalue
+					'element', // for allowing DOM manipulation
 				],
 			},
 		],
 		'no-tabs': 0,
+		'no-unused-expressions': ['error', { allowShortCircuit: true, allowTernary: true }],
+		'max-len': 0,
 	},
-	env: { browser: true },
 };
